af2f6332f559332c0865f2cded0e2deea57cfe91
nickolay <nickolay@nickolay-note.(none)>
2010-07-23 11:36:24 +0400

    - some progress on testing

--- t/020_fixtures.t.js
+++ t/020_fixtures.t.js
@@ -2,28 +2,33 @@ StartTest(function(t) {
     
     var async0 = t.beginAsync()
     
-    use([ 'KiokuJS.Test' ], function () {
+    //======================================================================================================================================================================================================================================================
+    t.diag('Sanity')
+    
+    t.ok(KiokuJS.Test, "KiokuJS.Test is here")
+    t.ok(KiokuJS.Backend.CouchDB, "KiokuJS.Backend.CouchDB is here")
+    
+    
+    new KiokuJS.Test({
+        t       : t,
         
-        //======================================================================================================================================================================================================================================================
-        t.diag('Sanity')
-        
-        t.ok(KiokuJS.Test, "KiokuJS.Test is here")
-        
-        new KiokuJS.Test({
-            t       : t,
+        init    : function () {
             
-            init    : function () {
-                return KiokuJS.connect({
-                    backend : new KiokuJS.Backend.Hash()
+            
+            
+            return KiokuJS.connect({
+                backend : new KiokuJS.Backend.CouchDB({
+                    host    : 'localhost',
+                    
+                    dbName  : 'KiokuJS.Backend.CouchDB.' + new Date().getTime()
                 })
-            }
-            
-        }).runAllFixtures().then(function () {
-            
-            t.endAsync(async0)
-            
-            t.done()
-            
-        }).now()
+            })
+        }
+        
+    }).runAllFixtures().andThen(function () {
+        
+        t.endAsync(async0)
+        
+        t.done()
     })
 })    
